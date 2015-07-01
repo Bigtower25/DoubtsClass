072912c8e4fe128279d412c68d064da046ba54eb
AnSoRu <angelsoler91@gmail.com>
2015-07-01 11:46:17 +0200

    Cambio de nombre de la bbdd en la conexion

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 	define('DB_SERVER','localhost');
-	define('DB_NAME','pruebas');
+	define('DB_NAME','aplicacionesWeb');
 	
 	$con = mysql_connect(DB_SERVER,'root','');
 	mysql_select_db(DB_NAME,$con);
